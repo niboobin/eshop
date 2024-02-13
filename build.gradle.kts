5d134748de4175151a90115ec14bd22d40618bf8
Obin Mandalika <obinnmandalikaa@gmail.com>
2024-02-13 18:35:05 +0700

    added sonarcloud as test coverage

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     jacoco
     id("org.springframework.boot") version "3.2.2"
     id("io.spring.dependency-management") version "1.1.4"
+    id("org.sonarqube") version "4.4.1.3373"
 }
 
 group = "id.ac.ui.cs.advprog"
@@ -25,6 +26,14 @@ configurations {
 
 repositories {
     mavenCentral()
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "niboobin_eshop")
+        property("sonar.organization", "niboobin")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
 
 dependencies {
